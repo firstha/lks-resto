2496672c4a8c8de14a2e5cd68af07999e97b7e78
firstha <fristhanoven@gmail.com>
2026-02-26 08:12:40 +0700

    up

--- app/Http/Controllers/Api/TableController.php
+++ app/Http/Controllers/Api/TableController.php
@@ -3,47 +3,44 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Table;
 use Illuminate\Http\Request;
 
 class TableController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        return response()->json(Table::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'table_number' => 'required',
+            'capacity' => 'required|numeric'
+        ]);
+
+        $table = Table::create($request->all());
+
+        return response()->json($table, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        return Table::findOrFail($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $table = Table::findOrFail($id);
+        $table->update($request->all());
+
+        return response()->json($table);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        Table::destroy($id);
+        return response()->json(['message' => 'Deleted']);
     }
 }
